6d83afdae2b6a0a7aaea1fb480b04a43a6eba1c5
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-08-13 21:20:55 +0100

    set up fetch to get data from openweather

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 require("dotenv").config();
 import "node-fetch";
-import express from "express";
+import express, { response } from "express";
+import fetch from "node-fetch";
 const app = express();
 const port = 3000;
 
 const apiKey = process.env.API_KEY;
 const cityName = "lagos";
 
-const fetchJSONData = `https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}`;
+const fetchJSONData = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  try {
+    const dataResponse = await fetch(fetchJSONData);
+
+    if (!dataResponse.ok) {
+      throw new Error(`HTTP ERROR: ${dataResponse.status}`);
+    }
+    const data = await dataResponse.json();
+    console.log(data);
+  } catch (error) {
+    console.error(`could not get data ${error}`);
+  }
 });
 
 app.listen(port, () => {
