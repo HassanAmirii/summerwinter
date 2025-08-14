726906fdf7401365fef19b61c9705e78af0c35be
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-08-14 14:50:28 +0100

    weather json retrieves now

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-require("node-fetch");
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -7,22 +6,22 @@ const port = 3000;
 const apiKey = process.env.API_KEY;
 const cityName = "lagos";
 
-const fetchJSONData = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-
 app.get("/", async (req, res) => {
   try {
+    const { default: fetch } = await import("node-fetch");
+    const fetchJSONData = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
     const dataResponse = await fetch(fetchJSONData);
 
     if (!dataResponse.ok) {
       throw new Error(`HTTP ERROR: ${dataResponse.status}`);
     }
     const data = await dataResponse.json();
-    console.log(data);
+    res.json(data);
   } catch (error) {
     console.error(`could not get data ${error}`);
+    res.status(500).json({ error: "Could not fetch data" });
   }
 });
-
 app.listen(port, () => {
   console.log(`summerwinter app listening on port ${port}`);
 });
