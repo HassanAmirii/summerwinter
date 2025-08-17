73d292a9cec9b845abafc7e5126d38072240b4de
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-08-17 22:32:12 +0100

    okay successfully implemented gettug weather data unto to implemeneting caching using redis

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const port = 3000;
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.get("/weather/:city", async (req, res) => {
+  console.log("city selected", req.params.city);
+  try {
+    const { default: fetch } = await import("node-fetch");
+    const cityName = req.params.city;
+    const OpenWeatherURl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+    const DataResponse = await fetch(OpenWeatherURl);
+    if (!DataResponse.ok) {
+      throw new Error(`http error: ${DataResponse.status}`);
+    } else {
+      const data = await DataResponse.json();
+      res.json(data);
+    }
+  } catch (error) {
+    console.error(`could not fetch data ${error}`);
+  }
 });
 
 app.listen(port, () => {
